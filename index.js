3019754d6b10dd58fa720bc97910726e604a510e
Charles <charlie.david@me.com>
2022-10-04 18:01:25 -0600

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+const strainImage = document.querySelector("#strain-image");
+const strainName = document.querySelector("#strain-name");
+const strainDescription = document.querySelector("#strain-description");
+const strainPrice = document.querySelector("#strain-price");
+const numberInCart = document.querySelector("#strain-in-cart");
+const menu = document.getElementById("strain-items")
+let currentStrain;
 
+const fetchReq1 = fetch ("http://localhost:3000/data")
+.then(res => res.json())
+
+
+const fetchReq2 = fetch("https://api.otreeba.com/v1/strains?count=5")
+.then(response => response.json())
+
+const allData = Promise.all([fetchReq1, fetchReq2])
+
+const newData = []
+
+function sliceData(allData){
+    newData.push(allData.slice(0))
+}
+console.log(newData)
+
+function buildMenu(strainObj){
+    const span = document.createElement('span')
+    span.addEventListener('click', () => strainAPI(strainObj),
+   strainDetails(localObj) )
+    span.textContent = strainObj.name
+    menu.append(span)
+}
+
+function strainAPI(strainObj) {
+    strainName.textContent = strainObj.name;
+}
+
+function strainDetails(localObj){
+    strainImage.src = localObj.image
+    strainDescription.textContent = localObj.lineage.value
+    strainPrice.textContent = `$${(localObj.price) + "per Oz"}`
+    numberInCart.textContent = localObj.number_in_bag
+
+}
+
+function setupCart() {
+    let addToCartForm = document.querySelector("#cart-form")
+    addToCartForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        currentDish.number_in_bag += parseInt(event.target["cart-amount"].value);
+        strainDish(currentDish);
+        addToCartForm.reset();
+    });
+}
